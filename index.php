9f795313839f6eb41deed2a69e0d284217e1deec
unknown <doanvantrong96@gmail.com>
2023-05-24 10:03:11 +0700

    debug

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
+echo 111;die;
 defined('YII_DEBUG') or define('YII_DEBUG',true);
-//defined('YII_ENV') or define('YII_ENV', 'dev');
+defined('YII_ENV') or define('YII_ENV', 'dev');
 require(__DIR__ . '/vendor/autoload.php');
 require(__DIR__ . '/vendor/yiisoft/yii2/Yii.php');
 require(__DIR__ . '/common/config/bootstrap.php');
