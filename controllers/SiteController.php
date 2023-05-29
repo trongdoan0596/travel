f85e35afa34d8943bc52ba6b239d0174d2f77cfb
unknown <doanvantrong96@gmail.com>
2023-05-29 15:10:40 +0700

    update

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -58,6 +58,7 @@ class SiteController extends Controller {
         return parent::beforeAction($action);
     }  
     public function actionIndex(){      
+        echo 333;die;
         $baseurl = 'https://authentiktravel.com/';
         $title   = 'Private Tours with a Local Travel Agency in Vietnam';
         $this->view->title = $title; 
