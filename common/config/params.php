6d182a2f44fe85bdb3a68d9c4464b848edc592b1
unknown <doanvantrong96@gmail.com>
2023-05-29 14:00:55 +0700

    update

--- common/config/params.php
+++
@@ -1,8 +0,0 @@
-<?php
-return array(    
-    'adminEmail'   => 'sales.authentiktravel@gmail.com',
-    'supportEmail' => 'support@authentiktravel.com',
-    'contactEmail' => 'sales.authentiktravel@gmail.com',
-    'commentEmail' => 'sales.authentiktravel@gmail.com',
-    'user.passwordResetTokenExpire' => 3600
-);
--- vendor/omgdef/yii2-multilingual-behavior/src/MultilingualQuery.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace omgdef\multilingual;
-
-use yii\db\ActiveQuery;
-
-class MultilingualQuery extends ActiveQuery
-{
-    use MultilingualTrait;
-}
